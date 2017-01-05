b209149eb25ba11127cd496f6c7fabafc05252b9
fadnihdr <fadnihdr@gmail.com>
2017-01-05 20:37:27 +0800

    added working main menu

--- src/main.java
+++ src/main.java
@@ -1,13 +1,51 @@
 import java.util.Scanner;
 
-/**
- * Created by fadni on 05/01/2017.
- */
 public class main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("How many players?");
-        int playerNum = sc.nextInt();
+    public static void main(String[] args) {
+        boolean inMenu = true;
+        while (inMenu == true) {
+            int choice = menu();
+            switch (choice) {
+                case 1: {
+                    int numPlayers = input("How many players");
+                    while (numPlayers > 5 || numPlayers == 0 || numPlayers < 3) {
+                        System.out.println("Invalid number");
+                        numPlayers = input("How many players. (3-5)");
+                    }
+                    inMenu = false;
+                    startGame(numPlayers);
+                    break;
+                }
+                case 2: {
+                    int exitChoice;
+                    exitChoice = input("Are you sure you want to exit?\n1. Yes\n2. No");
+                    if (exitChoice == 1) {
+                        System.out.println("Thankyou for playing!");
+                        System.exit(0);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
+    private static void startGame(int playerCount) {
+
+    }
+
+    private static int menu() {
+        System.out.println("Menu");
+        System.out.println("1. Start game\n2. Exit");
+        int choice = input("What would you like to do?");
+        return choice;
+    }
+
+    private static int input(String inputMessage) {
+        int number;
+        Scanner sc = new Scanner(System.in);
+        System.out.println(inputMessage + ": ");
+        number = sc.nextInt();
+        return number;
     }
 }
--- src/players.java
+++ src/players.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 public class players {
     private int playerCount;
     private int position;
